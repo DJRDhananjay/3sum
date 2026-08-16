1156f283ce5dbd9335b7e5e35a7c0296de6efdcf
DJRDhananjay <128344998+DJRDhananjay@users.noreply.github.com>
2026-08-16 07:26:13 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,23 +1,35 @@
 import java.util.*;
-public class Main
-{
-	public static void main(String[] args) {
-		int[] nums = {-1, 0, 1, 2, -1, -4};
-		ArrayList<ArrayList<Integer>>list=new ArrayList();
-		int target=0;
-		for(int i=0;i<nums.length;i++){
-		    for(int j=i+1;j<nums.length;j++){
-		        for(int k=j+1;k<nums.length;k++){
-		            if(nums[i]+nums[j]+nums[k]==target){
-		                ArrayList<Integer>triplet=new ArrayList();
-		                triplet.add(nums[i]);
-		                triplet.add(nums[j]);
-		                triplet.add(nums[k]);
-		                list.add(triplet);
-		            }
-		        }
-		    }
-		}
-		System.out.print(list);
-	}
+class Solution {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>>list=new ArrayList();
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]>nums[j]){
+                    int temp=nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                }
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                for(int k=j+1;k<nums.length;k++){
+                    if(nums[i]+nums[j]+nums[k]==0){
+                        List<Integer>triplet=new ArrayList();
+                        triplet.add(nums[i]);
+                        triplet.add(nums[j]);
+                        triplet.add(nums[k]);
+                        if(!list.contains(triplet)){
+                            list.add(triplet);
+                        }
+                    }
+                }
+            }
+        }
+        return list;
+    }
+    public static void main(String[] args){
+        int nums[]={-1,0,1,2,-1,-4};
+        System.out.print(threeSum(nums));
+    }
 }
